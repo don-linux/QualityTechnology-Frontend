8748d9a7ab08420ed1d47314820e340b6169eff1
Fernando <fernandoemiliolopez13@gmail.com>
2026-04-13 22:02:26 -0600

    Fixed ver foto route

--- src/utils/uploadUrl.js
+++ src/utils/uploadUrl.js
@@ -3,6 +3,6 @@ import { API_URL } from "./config";
 export function getUploadUrl(path) {
   if (!path) return null;
   const token = localStorage.getItem("token");
-  const url = path.startsWith("http") ? path : `${API_URL}${path}`;
+  const url = path.startsWith("http") ? path : `${API_URL}/${path.replace(/^\//, "")}`;
   return token ? `${url}?token=${token}` : url;
 }
